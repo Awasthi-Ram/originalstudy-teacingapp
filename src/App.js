edd7302a654aacdf29d4a3224d9a6b5f8812961a
ramawasthi <ramawasthi300@gmail.com>
2023-03-16 11:54:23 +0530

    ram home page navbar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import { AuthContext } from "./context/AuthContext";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Main from "./pages/main/main"
 import Register from "./pages/register/Register";
 
 function App() {
@@ -24,6 +25,10 @@ function App() {
     {
       path: "/register",
       element: <Register />,
+    },
+    {
+      path: "/main",
+      element: <Main/>
     },
     {
       path: "/",
---
+++ src/pages/main/main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../components/ramnavbar/ramNavbar'
+
+
+
+/* Add your custom styles below */
+
+function main() {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default main
