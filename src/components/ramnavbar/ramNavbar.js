d2f02aefcdd37b1276f3aaedd9774e62a5916c65
ramawasthi <ramawasthi300@gmail.com>
2023-03-16 14:12:58 +0530

    ramawasthi navbar v1.2

--- src/components/ramnavbar/ramNavbar.js
+++ src/components/ramnavbar/ramNavbar.js
@@ -1,37 +1,46 @@
-import React from 'react'
 
-function ramNavbar() {
+import React, { useState } from 'react';
+import "./ramNavbar.css";
+import logo from "./logo.png";
+import IconButton from '@mui/material/IconButton';
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SchoolIcon from '@mui/icons-material/School';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+function RamNavbar() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark"  data-bs-theme="dark">
-        <div className="container">
-          <a className="navbar-brand" href="#">Logo</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Services</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Portfolio</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
-              </li>
-            </ul>
-          </div>
+    <div className='navram'>
+    <nav className="navbar navbar-expand-lg navbar-light bg-blue shadow-md fixed-top" >
+      <div className="container">
+        <a className="navbar-brand me-auto order-first" href="#"><img className='logo me-auto order-first' src={logo} alt="logo"s/></a>
+        <button className="navbar-toggler" type="button" onClick={toggleNavbar}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`collapse navbar-collapse ${isExpanded ? 'show' : ''}`}>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#"><IconButton><AccountCircleIcon />Dashboard</IconButton></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#"><IconButton><EmojiPeopleIcon />social media</IconButton></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#"><IconButton><SchoolIcon />classroom</IconButton></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#"><IconButton><LiveTvIcon /> live classes</IconButton></a>
+            </li>
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
     </div>
-  )
+  );
 }
-
-export default ramNavbar
+export default RamNavbar
